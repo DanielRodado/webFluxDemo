d7c2cd711eb4d09f3d0f15ae784d48724818c365
DanielRodado <d4nielrodado@gmail.com>
2024-08-26 12:56:54 -0500

    Entidad 'Item'

---
+++ src/main/java/com/example/webfluxdemo/models/Item.java
@@ -0,0 +1,33 @@
+package com.example.webfluxdemo.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("items")
+public class Item {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
